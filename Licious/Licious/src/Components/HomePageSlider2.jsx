8ecbc0580a4ec14065c20762dd22c0fcb1f009a5
Chanchal Sharma <himanshu.hs662@gmail.com>
2022-12-16 20:09:40 +0530

    updateSlider

--- Licious/Licious/src/Components/HomePageSlider2.jsx
+++ Licious/Licious/src/Components/HomePageSlider2.jsx
@@ -47,19 +47,19 @@ const HomePageSlider2 =()=>{
           <div className={styles.HomPage_slider2_Child1}>
             <img src="https://d2407na1z3fc0t.cloudfront.net/inTheNews/35/inthenews_62aadde78bdae" alt="img1"/>
           </div>
-          <div className={styles.HomPage_slider2_Child2}>
+          {/* <div className={styles.HomPage_slider2_Child2}>
           <img src="https://d2407na1z3fc0t.cloudfront.net/inTheNews/40/inthenews_62aae21e9a38e" alt="img7"/>
-          </div>
+          </div> */}
           <div className={styles.HomPage_slider2_Child1}>
           <img src="https://d2407na1z3fc0t.cloudfront.net/inTheNews/35/inthenews_62aadde78bdae" alt="img2"/>
           </div>
-          <div className={styles.HomPage_slider2_Child3}>
+          {/* <div className={styles.HomPage_slider2_Child3}>
           <img src="https://d2407na1z3fc0t.cloudfront.net/inTheNews/42/inthenews_62aae2a6d9bc4" alt="img8"/>
-          </div>
+          </div> */}
 
-          <div className={styles.HomPage_slider2_Child2}>
+          {/* <div className={styles.HomPage_slider2_Child2}>
           <img src="https://d2407na1z3fc0t.cloudfront.net/inTheNews/40/inthenews_62aae21e9a38e" alt="img8"/>
-          </div>
+          </div> */}
           <div className={styles.HomPage_slider2_Child6}>
           <img src="https://d2407na1z3fc0t.cloudfront.net/inTheNews/45/inthenews_62aae4c4d8331" alt="img8"/>
           </div>
