51b3441a6763d3b13b3ab4be82a2da4a1b2fdaad
Aarif-shazZ <shazaarif0@gmail.com>
2022-12-17 15:33:00 +0530

    fw18_0632_day-4 done

--- Licious/Licious/src/Components/Navbar/Modals.jsx
+++ Licious/Licious/src/Components/Navbar/Modals.jsx
@@ -43,7 +43,7 @@ import {
                         <Button colorScheme='blue' mr={3} onClick={onClose}>
                             Close
                         </Button>
-                        <Button variant='ghost'>Secondary Action</Button>
+                        <Button variant='ghost'>Action</Button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
